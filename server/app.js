0977bda67ded554d4a7330718d7c80dedab8d820
Anant Anand <anantanand900@gmail.com>
2025-09-18 00:21:30 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(cors({
    origin: [
-    // 'http://localhost:5173',  // Local dev
+    'http://localhost:5173',  // Local dev
    ' https://flipkart-client-hd9b.onrender.com'
     // my deployed frontend 
     // URL render pr
