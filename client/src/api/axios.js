df52cf9a5b640b523a5fbda97cd2ef45c885e1fb
Anant Anand <anantanand900@gmail.com>
2025-09-17 21:49:34 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://flipkart-9l30.onrender.com',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
